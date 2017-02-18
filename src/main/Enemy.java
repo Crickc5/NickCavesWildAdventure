c1f8560c5ffa465609e76c2ff987b5740f8063b7
fancygama <stonetackle83@gmail.com>
2017-02-18 12:59:26 -0500

    Removed shit

--- src/main/Enemy.java
+++
@@ -1,9 +0,0 @@
-
-public class Enemy extends Entity {
-
-	public Enemy(boolean turnUsed, int[] hp, int[] atk, int[] def, int[] spd, Tile curTile) {
-		super(turnUsed, hp, atk, def, spd, curTile);
-		// TODO Auto-generated constructor stub
-	}
-
-}
